1b569ce07bb56047c7da199b8bf6733f1e2664f6
Aaron Witherspoon <a.witherspoon@gmail.com>
2021-11-02 19:15:20 -0500

    initial commit

---
+++ pages/_app.jsx
@@ -0,0 +1,18 @@
+/** @jsx jsx */ /** @jsxRuntime classic */
+import { jsx } from 'theme-ui'
+import { ThemeProvider} from 'theme-ui'
+import theme from '../theme';
+import Nav from '../src/components/nav';
+
+function MyApp({ Component, pageProps }) {
+  return (
+  <ThemeProvider theme={theme}>
+    <div>
+      <Nav />
+      <Component {...pageProps} />
+    </div>
+  </ThemeProvider>
+  )
+}
+
+export default MyApp
---
+++ src/components/browser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// console.log(document.body)
+
+const Browser = () => {
+   return (
+      <div>Browser component</div>
+   )
+}
+
+export default Browser
